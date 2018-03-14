51d5a251cddbc016c58672c9b172f9c639502b67
Zander Brown <ZanderBrown@users.noreply.github.com>
2018-03-14 19:36:51 +0000

    Support Python 2 as well as Python 3

--- nudatus.py
+++ nudatus.py
@@ -1,6 +1,13 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+import sys
 import token
 import tokenize
-from io import BytesIO, StringIO
+from io import BytesIO
+if sys.version_info < (3, 0):
+    from tokenize import generate_tokens as tokenizer
+else:
+    from tokenize import tokenize as tokenizer
 
 def mangle(text):
     """
@@ -12,7 +19,7 @@ def mangle(text):
     # Wrap the input script as a byte stream
     buff = BytesIO(text_bytes)
     # String stream for the mangled script
-    mangled = StringIO()
+    mangled = BytesIO()
 
     last_tok = token.INDENT
     last_line = -1
@@ -20,7 +27,7 @@ def mangle(text):
     last_line_text = ''
 
     # Build tokens from the script
-    tokens = tokenize.tokenize(buff.readline)
+    tokens = tokenizer(buff.readline)
     for t, text, (line_s, col_s), (line_e, col_e), line in tokens:
         # If this is a new line (except the very first)
         if line_s > last_line and last_line != -1:
@@ -29,14 +36,14 @@ def mangle(text):
             # If the last line ended in a '\' (continuation)
             if last_line_text.rstrip()[-1:] == '\\':
                 # Recreate it
-                mangled.write(' \\\n')
+                mangled.write(b' \\\n')
 
         # If this is a docstring comment
         if t == token.STRING and (last_tok == token.INDENT or
             last_tok == token.NEWLINE or last_tok == tokenize.NL):
             # Output number of lines corresponding those in
             # the docstring comment
-            mangled.write('\n' * (len(text.split('\n')) - 1))
+            mangled.write(b'\n' * (len(text.split('\n')) - 1))
         # Or is it a standard comment
         elif t == tokenize.COMMENT:
             # Plain comment, just don't write it
@@ -44,11 +51,12 @@ def mangle(text):
         else:
             # Recreate indentation, ideally we should use tabs
             if col_s > last_col:
-                mangled.write(" " * (col_s - last_col))
+                mangled.write(b' ' * (col_s - last_col))
             # This is a bit odd by without it the script seems
             # to be prefixed with utf-8, making it invalid
+            # If someone could tell me why, please do
             if text != 'utf-8' and last_line != -1:
-                mangled.write(text)
+                mangled.write(text.encode('utf-8'))
 
         last_tok = t
         last_col = col_e
@@ -56,11 +64,11 @@ def mangle(text):
         last_line_text = line
 
     # The flashing system expects bytes
-    result = mangled.getvalue()
+    result = mangled.getvalue().decode('utf-8')
     saved = len(text_bytes) - len(result)
     percent = saved / len(text_bytes) * 100
     return (result, saved, percent)
 
 with open('nudatus.py', 'r') as f:
     result, saved, percent = mangle(f.read())
-    print(result)
+    print(result, end='')
